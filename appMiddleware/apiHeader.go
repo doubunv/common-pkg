266d721c2b6252776f7f5a62b51c89c3e76174ce
mac <dev@qqqq>
2025-01-09 16:28:39 +0800

    checkhead

--- appMiddleware/apiHeader.go
+++ appMiddleware/apiHeader.go
@@ -67,10 +67,10 @@ func (m *ApiHeaderMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 			}
 		}
 
-		if h.Business == "" || h.Source == "" {
-			result.HttpErrorResult(r.Context(), w, errors.New("Head data error"))
-			return
-		}
+		//if h.Business == "" || h.Source == "" {
+		//	result.HttpErrorResult(r.Context(), w, errors.New("Head data error"))
+		//	return
+		//}
 
 		newCtx := headInfo.ContextHeadInLog(r.Context(), h)
 		newCtx = headInfo.HeadInMetadata(newCtx, *h)
