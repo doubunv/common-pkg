a539550defc2ebf6987cb9b4a8cae5a955b358c5
Zhuanz2密码0000 <Zhuanz2@MacBook-Pro.local>
2026-01-21 15:42:33 +0800

    mq

--- amqp/kafka/config/config.go
+++ amqp/kafka/config/config.go
@@ -18,7 +18,7 @@ type CustomerConfig struct {
 	GroupID     string
 	GroupTopics []string
 	StartOffset int64 `json:"StartOffset,default=-1"` //LastOffset  int64 = -1  or FirstOffset int64 = -2
-	Partition   int
+	Partition   int   `json:"partition,default=10"`
 }
 
 type ProviderConfig struct {
